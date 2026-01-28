6a4882ea9786879144cc23403791fbce1d73a44a
Eric López <eric.lopez.alonzo@gmail.com>
2026-01-28 15:11:56 -0600

    Add prerender routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,27 @@
+import { inject } from '@angular/core';
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { CompanyData } from './services/company-data';
+import { firstValueFrom } from 'rxjs';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'invoices/:companyId',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const companyService = inject(CompanyData);
+      const ids = await firstValueFrom(companyService.getCompanies());
+      return ids.map(c => ({ id: c.id.toString() }));
+    },
+  },
+  {
+    path : 'company/:id/clients',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const companyService = inject(CompanyData);
+      const ids = await firstValueFrom(companyService.getCompanies());
+      return ids.map(c => ({ id: c.id.toString() }));
+    }
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
