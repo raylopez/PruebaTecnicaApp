44040c1d69dae7e4fd21d7a4b89bb80d5e7dfc17
Eric López <eric.lopez.alonzo@gmail.com>
2026-01-28 13:36:40 -0600

    Update environment

--- src/app/services/company-data.ts
+++ src/app/services/company-data.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { Client } from '../models/client';
 import { Company } from '../models/company';
 
