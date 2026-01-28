968801ae328683324bdeedd732b6dbfe718dbbd2
Eric López <eric.lopez.alonzo@gmail.com>
2026-01-28 14:45:45 -0600

    Actualizar routes.server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,14 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path : 'invoices/:companyId',
-    renderMode: RenderMode.Client
-  },
-  {
-    path : 'company/:id/clients',
-    renderMode: RenderMode.Client
-  },
-  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
